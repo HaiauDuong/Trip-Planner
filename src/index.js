5d7188c03f9cfe0a934f5ab2b7c0f48f356796ca
Haiau Duong <duong.h.pgd@gmail.com>
2018-09-25 17:12:59 -0400

    earase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hayyyyyyyy from JS Zadddiiiez');
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoiaGtkdW9uZyIsImEiOiJjam1pN2ZkdWMwMjFhM3VvMXN5bjJ1M3pvIn0.-YAOilecCBqRy87_MP_GzQ";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
